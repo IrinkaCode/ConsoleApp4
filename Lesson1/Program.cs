cd283efd8f8a27e119a18946bbbd5ac79daebbb1
IrinkaCode <154838140+IrinkaCode@users.noreply.github.com>
2024-01-20 00:07:42 +0200

    домашка с чеком

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -1,2 +1,74 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// DZ Lesson5(2)
+
+
+
+const string Title1 = "1 КОФЕ СТАНДАРТНЫЙ";
+const double Price1 = 69.00;
+const double Number1 = 1.00;
+const double Amount1 = 69.00;
+Console.WriteLine("Название           Цена  Кол.Сумма");
+Console.WriteLine($"{Title1}   {Price1}  {Number1}   {Amount1}\t");
+
+const string Title2 = "НДС с рассчитанной \nставкой 18%";
+const double Amount2 = 10.53;
+Console.WriteLine($"{Title2}               {Amount2}");
+
+const string Title3 = "2 РУЛЕТ-СПРИНГ ТВ ИЗ ";
+const double Price3 = 70.00;
+const double Number3 = 1.00;
+const double Amount3 = 70.00;
+Console.WriteLine($"{Title3}{Price3}  {Number3}   {Amount3}\t");
+Console.WriteLine("КУ");
+const string Title4 = "НДС с рассчитанной\nставкой 10%";
+const double Amount4 = 6.36;
+Console.WriteLine($"{Title4}                {Amount4}\t");
+
+Console.WriteLine("ИТОГО:                   139.00");
+
+const double Means = 139.00;
+const double NDS18 = 10.53;
+const double NDS10 = 6.36;
+Console.WriteLine($"Электронные средства        {Means}");
+Console.WriteLine($"НДС итога чека с          {NDS18}");
+Console.WriteLine("рассчитанной ставкой\n18%");
+Console.WriteLine($"НДС итога чека с         {NDS10}");
+Console.WriteLine("рассчитанной ставкой\n10%");
+
+Console.WriteLine("ВИД НАЛОГООБЛОЖЕНИЯ: ОСН");
+const double REGNUMBER = 0000143873023483;
+Console.WriteLine($"РЕГ.НОМЕР ККГ:{REGNUMBER}");
+Console.WriteLine("ЗАВОД.№:");
+const double FN = 8710000100265168;
+const double FD = 6026;
+const double FPD = 3322278314;
+Console.WriteLine($"ФН №:{FN}");
+Console.WriteLine($"ФД №:{FD}");
+Console.WriteLine($"ФПД №:{FPD}");
+
+
+
+
+
+
+
+
+
+//Console.WriteLine("№ Название         Цена  Кол. Сумма");
+//Console.WriteLine("1 КОФЕ СТАНДАРТНЫЙ 69.00 1.00 69.00");
+//Console.WriteLine("НДС с расчитанной             10.53");
+//Console.WriteLine("ставкой 18%");
+
+//Console.WriteLine("РУЛЕТ-СПРИНГ ТВ ИЗ 70.00 1.00 70.00");
+//Console.WriteLine("КУ");
+//Console.WriteLine("НДС с рассчитанной             6.36");
+//Console.WriteLine("ставкой 10%");
+
+//Console.WriteLine("ИТОГО:                       139.00");
+
+
+
+
+
+
+
+
