1d22a360fc1ed74ebc27e741007c8ba3461db334
IrinkaCode <154838140+IrinkaCode@users.noreply.github.com>
2024-01-21 22:48:41 +0200

    урок 7(1)

---
+++ Lesson7/Program.cs
@@ -0,0 +1,138 @@
+﻿//Console.WriteLine("Привет");
+////вывод строки в консоль
+//Console.WriteLine(2);
+
+//int result = 2 + 2;
+//Console.WriteLine(result);
+//Console.WriteLine(result.ToString());
+//Console.WriteLine(int.Parse(result.ToString()));
+
+//int input=int.Parse(Console.ReadLine());
+//Console.WriteLine(input);
+
+
+//int First=int.Parse(Console.ReadLine());
+//Console.WriteLine(First);
+//int Second = int.Parse(Console.ReadLine());
+
+
+
+//Console.WriteLine("Укажите имя: ");
+//string name=Console.ReadLine();
+
+//Console.WriteLine("Укажите фамилию: ");
+//string surname=Console.ReadLine();
+
+//Console.WriteLine("Укажите возраст: ");
+//int age=int.Parse(Console.ReadLine());
+
+//string result = $"{name}\n{surname}\n{age}";
+//Console.WriteLine(result);
+
+
+//Console.WriteLine("20.01.2024");
+////создание конкретной даты 
+//DateTime date = new DateTime(2024,01,20);
+//Console.WriteLine($"{date}");
+
+//Создание текущей даты
+//DateTime currentDate=DateTime.Now;
+//Console.WriteLine(currentDate);
+
+
+//Console.WriteLine("Введите имя: ");
+//string name = Console.ReadLine();
+
+//Console.WriteLine("Укажите возраст: ");
+//int age = int.Parse(Console.ReadLine());
+
+//Console.WriteLine("Укажите город в котором живете: ");
+//string city =Console.ReadLine();
+
+//Console.WriteLine("Укажите профессию: ");
+//string occupation = Console.ReadLine();
+
+//Console.WriteLine("Введите место работы");
+//string placeOfWork=Console.ReadLine();
+
+//Console.WriteLine("Укажите семейное положение:");
+//string familyStatus = Console.ReadLine();
+
+//Console.WriteLine("Укажите наличие детей:");
+//string kids = Console.ReadLine();
+
+//Console.WriteLine("Укажите наличие животных:");
+//string animals=Console.ReadLine();
+
+//Console.WriteLine("\n\n\n\n*************\n\n\n\n");
+
+//string result=$"{name}\n{surname}\n{age}\n{city}\n{occupation}\n{placeOfWork}\n{familyStatus}\n{kids}\n{animals}";
+//Console.WriteLine(result);
+
+//Первый продукт
+Console.Write("Укажите продукт 1: ");
+string firstProductName=Console.ReadLine();
+
+Console.Write($"Укажите цену за {firstProductName}:");
+string firstProductPrice = Console.ReadLine();
+
+Console.WriteLine($"Укажите количество продукта {firstProductName}, по цене {firstProductPrice}:");
+string firstProductQuantity = Console.ReadLine();
+
+int Total = int.Parse.(firstProductPrice) * int.Parse(firstProductQuantity);
+
+//Второй продукт
+
+Console.Write("Укажите продукт 2: ");
+string secondProductName = Console.ReadLine();
+
+Console.Write($"Укажите цену за {secondProductName}:");
+string secondProductPrice = Console.ReadLine();
+
+Console.WriteLine($"Укажите количество продукта {secondProductName}, по цене {secondProductPrice}:");
+string secondProductQuantity = Console.ReadLine();
+
+int Total = int.Parse.(secondProductPrice) * int.Parse(secondProductQuantity);
+
+//Третий продукт
+
+Console.Write("Укажите продукт 3: ");
+string thirdProductName = Console.ReadLine();
+
+Console.Write($"Укажите цену за {thirdProductName}:");
+string thirdProductPrice = Console.ReadLine();
+
+Console.Write($"Укажите количество продукта {thirdProductName}, по цене {thirdProductPrice}:");
+string thirdProductQuantity = Console.ReadLine();
+
+
+//Четвертый продукт
+
+Console.Write("Укажите продукт 4: ");
+string fourthProductName = Console.ReadLine();
+
+Console.Write($"Укажите цену за {fourthProductName}:");
+string fourthProductPrice = Console.ReadLine();
+
+Console.Write($"Укажите количество продукта {fourthProductName}, по цене {fourthProductPrice}:");
+string fourthProductQuantity = Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
