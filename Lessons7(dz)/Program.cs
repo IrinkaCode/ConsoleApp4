e197ca4313d08b826b8bfa941ce8c070c47c98d2
IrinkaCode <154838140+IrinkaCode@users.noreply.github.com>
2024-01-22 21:36:30 +0200

    1

--- Lessons7(dz)/Program.cs
+++ Lessons7(dz)/Program.cs
@@ -1,7 +1,55 @@
 ﻿//Домашнее задание.Урок7
-//подсчет площади круга
+//1.подсчет площади круга
 
-Console.Write("Введите радиус окружности:");
-double radius = double.Parse(Console.ReadLine());
-const double PI = 3.14;
-Console.WriteLine($"{PI*radius*radius:F2}");
+//Console.Write("Введите радиус окружности:");
+//double radius = double.Parse(Console.ReadLine());
+//const double PI = 3.14;
+//Console.WriteLine($"{PI*radius*radius:F2}");
+
+//2.
+
+
+//Console.WriteLine("Введите ваше имя: ");
+//string name = Console.ReadLine();
+
+//Console.WriteLine("Введите вашу фамилию: ");
+//string surname=Console.ReadLine();
+
+//Console.WriteLine("Укажите возраст: ");
+//int age = int.Parse(Console.ReadLine());
+
+//Console.WriteLine("Укажите город в котором живете: ");
+//string city = Console.ReadLine();
+
+//Console.WriteLine("Укажите вашу профессию: ");
+//string occupation = Console.ReadLine();
+
+//Console.WriteLine("Введите место работы: ");
+//string placeOfWork = Console.ReadLine();
+
+//Console.WriteLine("Сколько лет вы работаете: ");
+//int years = int.Parse(Console.ReadLine());
+
+//Console.WriteLine("Укажите семейное положение: ");
+//string familyStatus = Console.ReadLine();
+
+//Console.WriteLine("Укажите наличие детей: ");
+//string kids = Console.ReadLine();
+
+//Console.WriteLine("Какое животное у вас есть: ");
+//string animals = Console.ReadLine();
+
+//Console.WriteLine("\n\n\n\n*************\n\n\n\n");
+
+//string result = $"{name}\n{surname}\n{age}\n{city}\n{occupation}\n{placeOfWork}\n{years}\n{familyStatus}\n{kids}\n{animals}";
+//Console.WriteLine(result);
+
+//3.Data Time
+
+//создание конкретной даты 
+//DateTime date = new DateTime(2024, 01, 20);
+//Console.WriteLine($"{date}");
+
+//Создание текущей даты
+DateTime currentDate = DateTime.Now;
+Console.WriteLine(currentDate);
