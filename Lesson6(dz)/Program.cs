927d8d6897bced335678674649ea7e6f615d214c
IrinkaCode <154838140+IrinkaCode@users.noreply.github.com>
2024-01-24 15:46:39 +0200

    2

--- Lesson6(dz)/Program.cs
+++ Lesson6(dz)/Program.cs
@@ -1,8 +1,40 @@
-﻿//Урок 2
+﻿//Урок 1
 
-string myString = "254";
+//string number = "28";
+//byte numberByte = byte.Parse(number);
+//Console.WriteLine(number);
+
+//nint numberNint = numberByte;
+//Console.WriteLine(numberByte);
+
+//int numberInt = (int)numberNint;
+//Console.WriteLine(numberNint);
+
+//long numberLong = (long)numberInt;
+//Console.WriteLine(numberInt);
+
+//double numberDouble = (double)numberLong;
+//Console.WriteLine(numberLong);
+//Console.WriteLine(numberDouble);
+
+///Console.WriteLine($"{number}\n{numberByte}\n{numberNint}\n{numberInt}\n{numberLong}\n{numberDouble}");
+
+
+
+//Урок 2
+
+//string myString = "254";
+//int number = int.Parse(myString);
+//Console.WriteLine(number.GetType());
+//string newString = number.ToString();
+//Console.WriteLine(newString.GetType());
+//Console.WriteLine(newString);
+
+
+string myString = "6742";
 int number = int.Parse(myString);
 Console.WriteLine(number.GetType());
 string newString = number.ToString();
 Console.WriteLine(newString.GetType());
 Console.WriteLine(newString);
+
