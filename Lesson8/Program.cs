40e3fe941cd5d5fc72d6c5cc8c63815787785a36
IrinkaCode <154838140+IrinkaCode@users.noreply.github.com>
2024-01-25 21:36:19 +0200

    2

--- Lesson8/Program.cs
+++ Lesson8/Program.cs
@@ -26,17 +26,26 @@
 //Console.WriteLine(x);
 
 
-int a = 28;
-int b = 24;
-int c = 18;
-int d = c - (a - b);
-Console.WriteLine(d);
-int e = 24 - (a - c);
-Console.WriteLine(e);
+//int a = 28;
+//int b = 24;
+//int c = 18;
+//int d = c - (a - b);
+//Console.WriteLine(d);
+//int e = 24 - (a - c);
+//Console.WriteLine(e);
 
+//decimal all = 56000;
+//decimal part = all / 8;
+//Console.WriteLine($"{part*5}:{part*2}:{part}");
 
+//double s = 240;
+//double v1 = 3;
+//double v2 = 2;
+//Console.WriteLine(Math.Abs((s / v1) - (s / v2)));
 
-
+//Console.WriteLine("Введите x:");
+//int x= int.Parse(Console.ReadLine());
+//Console.WriteLine(x-x%100/10*10);
 
 
 
