5dc9d0c566ce958b9df63d239c02831dab3e35c3
IrinkaCode <154838140+IrinkaCode@users.noreply.github.com>
2024-01-25 21:05:10 +0200

    1

--- Lesson8/Program.cs
+++ Lesson8/Program.cs
@@ -11,4 +11,34 @@
 //Console.WriteLine(x++*2);
 //Console.WriteLine(x);
 //Console.WriteLine(++x*2);
- 
+
+//int x = 0;
+//Console.WriteLine(x);
+//x = x + 46;
+//Console.WriteLine(x);
+//x = x + 12;
+//Console.WriteLine(x);
+//x = x + 4;
+//Console.WriteLine(x);
+//x = x + 8;
+//Console.WriteLine(x);
+//x = x + 15;
+//Console.WriteLine(x);
+
+
+int a = 28;
+int b = 24;
+int c = 18;
+int d = c - (a - b);
+Console.WriteLine(d);
+int e = 24 - (a - c);
+Console.WriteLine(e);
+
+
+
+
+
+
+
+
+
