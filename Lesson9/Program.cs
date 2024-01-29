34a24d46162d9a3941137c3dc6bdade0472ded2c
IrinkaCode <154838140+IrinkaCode@users.noreply.github.com>
2024-01-29 17:01:10 +0200

    2

--- Lesson9/Program.cs
+++ Lesson9/Program.cs
@@ -1,3 +1,10 @@
-﻿int a = 16;
-int b = 7;
-Console.WriteLine(a&b);
+﻿//int a = 16;
+//int b = 7;
+//Console.WriteLine(a&b);
+
+Console.Write("Введите число:");
+int a =int.Parse(Console.ReadLine());
+if (a % 2 >= 0)
+    Console.WriteLine("Четное");
+else
+    Console.WriteLine("Нечетное");
