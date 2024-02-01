93b289c22c6648996c5e4f93ed2d4098b2e485b6
IrinkaCode <154838140+IrinkaCode@users.noreply.github.com>
2024-02-01 22:07:28 +0200

    /1

--- Lesson9/Program.cs
+++ Lesson9/Program.cs
@@ -2,9 +2,50 @@
 //int b = 7;
 //Console.WriteLine(a&b);
 
-Console.Write("Введите число:");
-int a =int.Parse(Console.ReadLine());
-if (a % 2 >= 0)
-    Console.WriteLine("Четное");
-else
-    Console.WriteLine("Нечетное");
+//Console.Write("Введите число:");
+//int a =int.Parse(Console.ReadLine());
+//if (a % 2 >= 0)
+//    Console.WriteLine("Четное");
+//else
+//    Console.WriteLine("Нечетное");
+
+
+//Console.WriteLine(249^8);
+//Console.WriteLine(Convert.ToString(249^8,2));
+
+//Int16 a = 256;
+//Console.WriteLine(Convert.ToString(a,2));
+//Console.WriteLine(Convert.ToString(~a,2));
+
+
+//Console.WriteLine(2<<1);
+//Console.WriteLine(16>>3);
+
+//Логические операторы
+//int x = 1;
+//int y = -1;
+//Console.WriteLine(x*x+y*y<=4);
+//x = 1;
+//y = 2;
+//Console.WriteLine(x>=0||y*y!=4);
+
+//int x = 2;
+//int y = 1;
+//Console.WriteLine(x*y!=0||y<x);
+
+//3.31
+//Console.Write("Введите N:");
+//int N=int.Parse(Console.ReadLine());
+//Console.WriteLine(N % 5 == 0 || N % 7 == 0);
+//Console.WriteLine(N % 4 == 0 && N % 10 != 0);
+
+//Console.Write("Введите x:");
+//Console.Write("Введите y:");
+//double x = double.Parse(Console.ReadLine());
+//double y = double.Parse(Console.ReadLine());
+//Console.WriteLine((x >= 2) && (y >= 1 || y <= -1.5));
+
+//Console.WriteLine("Введите y:");
+//double y=double.Parse(Console.ReadLine());
+//Console.WriteLine((y >= 1) || (y > -4) && (y < -3));
+
