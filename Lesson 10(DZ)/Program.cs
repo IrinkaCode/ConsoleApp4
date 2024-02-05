ab0d4afdde67688787dbe5f2f655b54c44c899cf
IrinkaCode <154838140+IrinkaCode@users.noreply.github.com>
2024-02-05 23:16:07 +0200

    2

--- Lesson 10(DZ)/Program.cs
+++ Lesson 10(DZ)/Program.cs
@@ -1,7 +1,14 @@
-﻿//Урок 10 
+﻿// Тема 10
+//Урок 1
+Console.Write("Введите значение радиуса окружности: ");
+double radius = double.Parse(Console.ReadLine());
+Console.WriteLine($"Диаметр окружности равен: {Math.Abs(radius * 2)}");
+Console.WriteLine($"Площадь окружности равна: {Math.Abs(radius * radius * Math.PI):F2}");
+ 
 
-Console.Write("Введите значение катета: ");
-double a = double.Parse(Console.ReadLine());
-Console.Write("Введите значение гипотенузы:");
-double b = double.Parse(Console.ReadLine());
-Console.WriteLine($"{Math.Sqrt(b * b - a * a):F2}");
+//Урок 2
+//Console.Write("Введите значение катета: ");
+//double a = double.Parse(Console.ReadLine());
+//Console.Write("Введите значение гипотенузы:");
+//double b = double.Parse(Console.ReadLine());
+//Console.WriteLine($"{Math.Sqrt(b * b - a * a):F2}");
