1f0aa313be78c7421f634fc855380ada20f4af8b
IrinkaCode <154838140+IrinkaCode@users.noreply.github.com>
2024-02-05 22:14:18 +0200

    1

--- Lesson 10(DZ)/Program.cs
+++ Lesson 10(DZ)/Program.cs
@@ -4,4 +4,4 @@ Console.Write("Введите значение катета: ");
 double a = double.Parse(Console.ReadLine());
 Console.Write("Введите значение гипотенузы:");
 double b = double.Parse(Console.ReadLine());
-Console.WriteLine($"{Math.Sqrt(b - a):F2}");
+Console.WriteLine($"{Math.Sqrt(b * b - a * a):F2}");
