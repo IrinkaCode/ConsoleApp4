c0bd25defa44775d28abcf915ca41abe18b32cdb
IrinkaCode <154838140+IrinkaCode@users.noreply.github.com>
2024-02-22 21:04:39 +0200

    2

---
+++ Lesson15/Program.cs
@@ -0,0 +1,55 @@
+﻿//одномерные массивы
+
+//int[] mas1 = new int[] { 4, 7, 3, 8, 2 };//инициализация
+//int[] mas2 = new int[5];//ввод с клавиатуры
+//Console.WriteLine("Введите элементы массива: ");
+//for  (int i = 0; i < mas2.Length; i++)
+//{
+//    mas2[i]=int.Parse(Console.ReadLine());
+//}
+//
+//    Console.Write(mas2[i]+" ");
+//Console.WriteLine();
+
+//генерация по формуле
+//int[] mas3 = new int[10];
+//for (int i = 0; i < mas3.Length; i++)
+//{
+//    mas3[i] = 2 * i + 1;
+//    Console.Write(mas3[i]+" ");
+//}
+//Console.WriteLine();
+
+////генератор случайных чисел
+//int[] mas4 = new int[10];
+//Random random = new Random();
+//for (int i = 0; i < mas4.Length; i++)
+//{
+//    mas4[i]=random.Next(10,100);
+//    Console.Write(mas4[i]+" ");
+//}
+//Console.WriteLine();
+
+//double[] mas5 = new double[20];
+//for (int i = 0; i < mas5.Length; i++)
+//{
+//    mas5[i]=random.NextDouble()*100;
+//    Console.WriteLine($"{mas5[i]:F2} ");
+//}
+//Console.WriteLine();
+
+
+//Console.Write("Введите размер массива: ");
+//int n=int.Parse(Console.ReadLine());
+//int[] mas=new int[n];
+//Random random=new Random();
+//int sum = 0;
+//for (int i = 0; i < mas.Length; i++)
+//{
+//    mas[i]= random.Next(10,100);
+//    Console.Write(mas[i]+" ");
+//    sum += mas[i];
+//}
+//Console.WriteLine();
+//Console.WriteLine("sum="+sum);
+
