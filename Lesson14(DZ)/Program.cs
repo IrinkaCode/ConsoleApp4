e5491d3c19c86a5f4bfca44710cff1c7603e52a5
IrinkaCode <154838140+IrinkaCode@users.noreply.github.com>
2024-03-01 21:40:33 +0200

    1

--- Lesson14(DZ)/Program.cs
+++ Lesson14(DZ)/Program.cs
@@ -1,1 +1,38 @@
-﻿//Урок 1
+﻿//Урок 2
+//Random random=new Random();
+//int temperature = 0;
+//for (int i = 1; i <= temperature; i++)
+//{
+//    int temp = random.Next(1, 101);
+//    Console.Write(temperature + " ");
+
+//    int temp= int.Parse(Console.ReadLine());
+//    Console.WriteLine("Введите текущую температуру: ");
+
+
+//    if (temperature >= 90) break;
+//    Console.WriteLine("Устройство остановлено");
+//    Console.WriteLine($"Критическая температура: {temperature}");
+//}
+
+
+//Random random = new Random();
+//int temperature = random.Next(1, 101);
+//Console.WriteLine($"Сгенерированная температура: {temperature}");
+//for (int i = 1; i <= temperature; i++)
+//{
+//    if (temperature > 90) break;
+//    Console.WriteLine("Устройство остановлено");
+//    Console.WriteLine($"Критическая температура: {temperature}");
+//}
+
+Random random = new Random();
+int temperature = random.Next(1, 101);
+
+for (int i = 1; i <= temperature; i++)
+{
+    if (temperature > 90)
+        Console.WriteLine($"Критическая температура: {temperature}"); break;
+    else
+        Console.WriteLine($"Текущая температура: {temperature}");
+}     
