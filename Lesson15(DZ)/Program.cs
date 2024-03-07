8aeb407932f7828cf95b95417a2fb8df78b555a0
IrinkaCode <154838140+IrinkaCode@users.noreply.github.com>
2024-03-07 16:19:13 +0200

    1

--- Lesson15(DZ)/Program.cs
+++ Lesson15(DZ)/Program.cs
@@ -78,41 +78,44 @@
 
 //Урок 4
 //Создать 4 массива. Заполнить их случайными числами.Конечным результатом должен быть, выведенный на консоль, массив,хранящий следующее:Произведение средних арифметических (1и2) и (3и4) массивов поиндексно.
-//Random random = new Random();
-//int[] mas1 = new int[1];
-//int[] mas2 = new int[1];
-//int[] mas3 = new int[1];
-//int[] mas4 = new int[1];
-//for (int i = 0; i < mas1.Length; i++)
-//{
-//    mas1[i] = random.Next(1,10);
-//    Console.Write(mas1[i] + " ");
-//}
-//for (int i = 0; i < mas2.Length; i++)
-//{
-//    mas2[i] = random.Next(1,10);
-//    Console.Write(mas2[i] + " ");
-//}
-//for (int i = 0; i < mas3.Length; i++)
-//{
-//    mas3[i] = random.Next(1,10);
-//    Console.Write(mas3[i] + " ");
-//}
-//for (int i = 0; i < mas4.Length; i++)
-//{
-//    mas4[i] = random.Next(1,10);
-//    Console.Write(mas4[i] + " ");
-//}
-//Console.WriteLine();
+Random random = new Random();
+int[] mas1 = new int[1];
+int[] mas2 = new int[1];
+int[] mas3 = new int[1];
+int[] mas4 = new int[1];
+for (int i = 0; i < mas1.Length; i++)
+{
+    mas1[i] = random.Next(1, 10);
+    Console.Write(mas1[i] + " ");
+}
+for (int i = 0; i < mas2.Length; i++)
+{
+    mas2[i] = random.Next(1, 10);
+    Console.Write(mas2[i] + " ");
+}
+for (int i = 0; i < mas3.Length; i++)
+{
+    mas3[i] = random.Next(1, 10);
+    Console.Write(mas3[i] + " ");
+}
+for (int i = 0; i < mas4.Length; i++)
+{
+    mas4[i] = random.Next(1, 10);
+    Console.Write(mas4[i] + " ");
+}
+Console.WriteLine();
 
-//double avg12 = ((mas1.Sum() + mas2.Sum()) / 2);
-//double avg34 = ((mas3.Sum() + mas4.Sum()) / 2);
-//double productMas = avg12 * avg34;
+double avg12 = ((mas1.Sum() + mas2.Sum()) / 2);
+double avg34 = ((mas3.Sum() + mas4.Sum()) / 2);
+double productMas = avg12 * avg34;
 
-//Console.WriteLine($"Среднее арифметическое 1 и 2 массивов = {avg12}");
-//Console.WriteLine($"Среднее арифметическое 3 и 4 массивов = {avg34}");
-
-//Console.WriteLine($"Произведение средних арифметических (1 и 2) и (3 и 4) массивов = {productMas}"); 
-
-
-
+Console.WriteLine("1 массив: " + string.Join(",", mas1));
+Console.WriteLine("2 массив: " + string.Join(",", mas2));
+Console.WriteLine($"Среднее арифметическое 1 и 2 массивов = {avg12}");
+Console.WriteLine("3 массив: " + string.Join(",", mas3));
+Console.WriteLine("4 массив: " + string.Join(",", mas4));
+Console.WriteLine($"Среднее арифметическое 3 и 4 массивов = {avg34}");
+Console.WriteLine($"Произведение средних арифметических:");
+Console.WriteLine($"Среднее арифметическое 1 и 2 массивов = {avg12}");
+Console.WriteLine($"Среднее арифметическое 3 и 4 массивов = {avg34}");
+Console.WriteLine($"Произведение = {productMas}");
