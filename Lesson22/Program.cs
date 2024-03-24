a5fd7bef43f3624c389db1e4f028ce395abc7481
IrinkaCode <154838140+IrinkaCode@users.noreply.github.com>
2024-03-24 20:31:49 +0200

    1

--- Lesson22/Program.cs
+++ Lesson22/Program.cs
@@ -3,14 +3,13 @@
 //Волчий остров
 Random random = new Random();
 char[] rabbits = new char[random.Next(10, 20)];
-int[,] rabbitGeo = new int[rabbits.Length, 3];
+int[,] rabbitGeo = new int[rabbits.Length, 2];
 for (int i = 0; i < rabbitGeo.GetLength(0); i++)
 {
     int k = random.Next(20);
     int m = random.Next(20);
-    rabbitGeo[i, 0] = i;
-    rabbitGeo[i, 1] = k;
-    rabbitGeo[i, 2] = m;
+    rabbitGeo[i, 0] = k;
+    rabbitGeo[i, 1] = m;
 }
 char[] wolfs = new char[random.Next(5) + 2];
 char[] female = new char[random.Next(5) + 2];
@@ -21,7 +20,6 @@ int fCount = female.Length;
 bool play = true;
 do
 {
-
     Console.Clear();
     for (int i = 0; i < square.GetLength(0); i++)
     {
@@ -32,14 +30,27 @@ do
     }
     for (int i = 0; i < rabbits.Length; i++)
     {
-        int k = random.Next(20);
-        int m = random.Next(20);
+        int k;
+        do
+        {
+            k = rabbitGeo[i, 0] + random.Next(-8, 8);
+        }
+        while (k < 0 || k > 19);
+        int m;
+        do
+        {
+            m = rabbitGeo[i, 1] + random.Next(-8, 8);
+        }
+        while (m < 0 || m > 19);
         square[k, m] = 'R';
-        rabbitGeo[i, 0] = i;
-        rabbitGeo[i, 1] = k;
-        rabbitGeo[i, 2] = m;
+        rabbitGeo[i, 0] = k;
+        rabbitGeo[i, 1] = m;
         int probably = (int)(random.NextDouble() * 100);
-        if (probably == 20) Array.Resize(ref rabbits, rabbits.Length + 1);
+        if (probably == 20)
+        {
+            Array.Resize(ref rabbits, rabbits.Length + 1);
+            rabbitGeo = new int[rabbits.Length, 2];
+        }
     }
     for (int i = 0; i < wolfs.Length; i++)
     {
---
+++ Lesson23/Program.cs
@@ -0,0 +1,112 @@
+﻿//игра змейка
+char[,] grid = new char[18, 18];
+char[] snake = new char[4];
+Array.Fill(snake, 'X');
+int[,] geoSnake = new int[4, 2];
+for (int i = 0; i < geoSnake.GetLength(1); i++)
+{
+    geoSnake[0, i] = snake.Length - i - 1;
+}
+
+char geo = 'd';
+do
+{
+    Console.Clear();
+    for (int i = 0; i < grid.GetLength(0); i++)
+    {
+        for (int j = 0; j < grid.GetLength(1); j++)
+        {
+            grid[i, j] = '.';
+        }
+    }
+    switch (geo)
+    {
+        case 'd':
+        case 'в':
+            {
+                for (int i = snake.Length - 1; i > 0; i--)
+                {
+                    geoSnake[i, 0] = geoSnake[i - 1, 0];
+                    geoSnake[i, 1] = geoSnake[i - 1, 1];
+                }
+                int temp = ++geoSnake[0, 1];
+                geoSnake[0, 1] = temp;
+                for (int i = 0; i < snake.Length; i++)
+                {
+                    int x = geoSnake[i, 0];
+                    int y = geoSnake[i, 1];
+                    grid[x, y] = 'X';
+                }
+            }
+            break;
+        case 's':
+        case 'ы':
+            {
+                for (int i = snake.Length - 1; i > 0; i--)
+                {
+                    geoSnake[i, 0] = geoSnake[i - 1, 0];
+                    geoSnake[i, 1] = geoSnake[i - 1, 1];
+                }
+                int temp = ++geoSnake[0, 0];
+                geoSnake[0, 0] = temp;
+                for (int i = 0; i < snake.Length; i++)
+                {
+                    int x = geoSnake[i, 0];
+                    int y = geoSnake[i, 1];
+                    grid[x, y] = 'X';
+                }
+            }
+            break;
+        case 'a':
+        case 'ф':
+            {
+                for (int i = snake.Length - 1; i > 0; i--)
+                {
+                    geoSnake[i, 0] = geoSnake[i - 1, 0];
+                    geoSnake[i, 1] = geoSnake[i - 1, 1];
+                }
+                int temp = --geoSnake[0, 1];
+                geoSnake[0, 1] = temp;
+                for (int i = 0; i < snake.Length; i++)
+                {
+                    int x = geoSnake[i, 0];
+                    int y = geoSnake[i, 1];
+                    grid[x, y] = 'X';
+                }
+            }
+            break;
+        case 'w':
+        case 'ц':
+            {
+                for (int i = snake.Length - 1; i > 0; i--)
+                {
+                    geoSnake[i, 0] = geoSnake[i - 1, 0];
+                    geoSnake[i, 1] = geoSnake[i - 1, 1];
+                }
+                int temp = --geoSnake[0, 0];
+                geoSnake[0, 0] = temp;
+                for (int i = 0; i < snake.Length; i++)
+                {
+                    int x = geoSnake[i, 0];
+                    int y = geoSnake[i, 1];
+                    grid[x, y] = 'X';
+                }
+            }
+            break;
+    }
+
+    for (int i = 0; i < grid.GetLength(0); i++)
+    {
+        for (int j = 0; j < grid.GetLength(1); j++)
+        {
+            Console.Write(grid[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    if (Console.KeyAvailable)
+    {
+        geo = Console.ReadKey().KeyChar;
+    }
+    Thread.Sleep(500);
+}
+while (true);
