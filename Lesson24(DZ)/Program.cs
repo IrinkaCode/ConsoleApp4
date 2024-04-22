c526254d8d59012176bb9d0f2ba3087e6e76d4bb
IrinkaCode <pil02022012@gmail.com>
2024-04-22 16:12:34 +0200

    1

--- Lesson24(DZ)/Program.cs
+++ Lesson24(DZ)/Program.cs
@@ -2,38 +2,28 @@
 //Дано слово, состоящее только из строчных букв. Проверьте, является ли это слово палиндромом.
 //Выведите Is a palindrom если слово таковым является и Not a palindrom в противном случае.
 
-//Console.Write("Введите слово: ");
-//string word = Console.ReadLine();
+Console.Write("Введите слово: ");
+string UserWord = Console.ReadLine();
 
-//if (IsPalindrome(word))
-//{
-//    Console.WriteLine("Is a palindrom");
-//}
-//else
-//{
-//    Console.WriteLine("Not a palindrom");
-//}
-
-
-//bool IsPalindrome(string word)
-//{
-//    int left = 0;
-//    int right = word.Length - 1;
-
-//    while (left < right)
-//    {
-//        if (word[left] != word[right])
-//        {
-//            return false;
-//        }
-
-//        left++;
-//        right--;
-//    }
-
-//    return true;
-//}
-
+IsPalindrom(UserWord,0,UserWord.Length-1);
+void IsPalindrom(string UserWord,int minIndex,int maxIndex)
+{
+    if (UserWord[minIndex] == UserWord[maxIndex])
+    {
+        minIndex++;
+        maxIndex--;
+        Console.WriteLine($"{UserWord} - Is a palindrom.");
+    }
+    else
+    {
+        Console.WriteLine($"{UserWord} - Not a palindrom.");
+    }
+    if (UserWord[minIndex] == UserWord[maxIndex] || UserWord[minIndex] != UserWord[maxIndex])
+    {
+        return;
+    }
+    IsPalindrom(UserWord, minIndex, maxIndex - 1);
+}
 
 
 //Урок 2
@@ -41,24 +31,21 @@
 //На ввод берется число от пользователя и передается в функцию.
 //Функция должна выводить все нечетные числа от 1 до этого числа не включительно и делать это рекурсивно.
 
-//void Main()
+//Console.Write("Введите число: ");
+//int max = int.Parse(Console.ReadLine());
+//Print(1, max-1);
+
+//void Print(int min, int max)
 //{
-Console.Write("Введите число: ");
-int max = int.Parse(Console.ReadLine());
-Print(1, max-1);
+//    if (min > max)
+//    {
+//        return;
+//    }
+
+//    if (min % 2 != 0)
+//    {
+//        Console.Write(min + " ");
+//    }
+
+//    Print(min + 1, max);
 //}
-
-void Print(int min, int max)
-{
-    if (min > max)
-    {
-        return;
-    }
-
-    if (min % 2 != 0)
-    {
-        Console.Write(min + " ");
-    }
-
-    Print(min + 1, max);
-}
