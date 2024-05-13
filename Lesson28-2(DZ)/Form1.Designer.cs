e02b9fcaf01afbb641df4abdb3a75dde4d8efc3f
IrinkaCode <pil02022012@gmail.com>
2024-05-14 00:34:16 +0200

    14.05

---
+++ Lesson28-2(DZ)/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Lesson28_2_DZ_
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            listBox1 = new ListBox();
+            listBox2 = new ListBox();
+            listBox3 = new ListBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            textBox1.Location = new Point(12, 3);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(209, 34);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "Имя пользователя";
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            textBox2.Location = new Point(227, 3);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(150, 34);
+            textBox2.TabIndex = 1;
+            textBox2.Text = "Логин:";
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            textBox3.Location = new Point(383, 3);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(207, 34);
+            textBox3.TabIndex = 2;
+            textBox3.Text = "Пароль:";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(12, 43);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(209, 104);
+            listBox1.TabIndex = 3;
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.Location = new Point(227, 43);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(150, 264);
+            listBox2.TabIndex = 4;
+            // 
+            // listBox3
+            // 
+            listBox3.FormattingEnabled = true;
+            listBox3.Location = new Point(383, 43);
+            listBox3.Name = "listBox3";
+            listBox3.Size = new Size(207, 264);
+            listBox3.TabIndex = 5;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            button1.Location = new Point(12, 153);
+            button1.Name = "button1";
+            button1.Size = new Size(209, 154);
+            button1.TabIndex = 6;
+            button1.Text = "Сгенерировать\r\nпароль\r\n";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(601, 317);
+            Controls.Add(button1);
+            Controls.Add(listBox3);
+            Controls.Add(listBox2);
+            Controls.Add(listBox1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Менеджер паролей";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private ListBox listBox1;
+        private ListBox listBox2;
+        private ListBox listBox3;
+        private Button button1;
+    }
+}
