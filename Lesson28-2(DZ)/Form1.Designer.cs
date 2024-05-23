b14093fa7109803af18e1de1b02f31e491a26c3e
IrinkaCode <pil02022012@gmail.com>
2024-05-23 16:21:52 +0200

    23.05

--- Lesson28-2(DZ)/Form1.Designer.cs
+++ Lesson28-2(DZ)/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Lesson28_2_DZ_
 {
-    partial class TextBox
+    partial class Form1
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,119 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
-            textBox1 = new System.Windows.Forms.TextBox();
-            textBox2 = new System.Windows.Forms.TextBox();
-            textBox3 = new System.Windows.Forms.TextBox();
-            listBox1 = new ListBox();
-            ComboBox = new ListBox();
-            ListBox = new ListBox();
-            Button = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            button1 = new Button();
             button2 = new Button();
+            tbusserlog = new TextBox();
+            lblog = new ListBox();
+            lbpass = new ListBox();
             SuspendLayout();
             // 
             // textBox1
             // 
-            textBox1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
-            textBox1.Location = new Point(12, 3);
+            textBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            textBox1.Location = new Point(12, 12);
+            textBox1.Multiline = true;
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(209, 34);
+            textBox1.Size = new Size(200, 35);
             textBox1.TabIndex = 0;
-            textBox1.Text = "Имя пользователя";
+            textBox1.Text = "Имя пользователя:";
             // 
             // textBox2
             // 
-            textBox2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
-            textBox2.Location = new Point(227, 3);
+            textBox2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            textBox2.Location = new Point(218, 13);
+            textBox2.Multiline = true;
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(150, 34);
+            textBox2.Size = new Size(170, 34);
             textBox2.TabIndex = 1;
             textBox2.Text = "Логин:";
             // 
             // textBox3
             // 
-            textBox3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
-            textBox3.Location = new Point(383, 3);
+            textBox3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            textBox3.Location = new Point(394, 12);
+            textBox3.Multiline = true;
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(207, 34);
+            textBox3.Size = new Size(206, 34);
             textBox3.TabIndex = 2;
             textBox3.Text = "Пароль:";
             // 
-            // listBox1
+            // button1
             // 
-            listBox1.FormattingEnabled = true;
-            listBox1.Location = new Point(12, 43);
-            listBox1.Name = "listBox1";
-            listBox1.Size = new Size(209, 104);
-            listBox1.TabIndex = 3;
-            // 
-            // ComboBox
-            // 
-            ComboBox.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            ComboBox.FormattingEnabled = true;
-            ComboBox.ItemHeight = 28;
-            ComboBox.Items.AddRange(new object[] { "FirstUser", "SecondUser", "ThirdUser" });
-            ComboBox.Location = new Point(227, 43);
-            ComboBox.Name = "ComboBox";
-            ComboBox.Size = new Size(150, 256);
-            ComboBox.TabIndex = 4;
-            // 
-            // ListBox
-            // 
-            ListBox.FormattingEnabled = true;
-            ListBox.Location = new Point(383, 43);
-            ListBox.Name = "ListBox";
-            ListBox.Size = new Size(207, 264);
-            ListBox.TabIndex = 5;
-            // 
-            // Button
-            // 
-            Button.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
-            Button.Location = new Point(12, 153);
-            Button.Name = "Button";
-            Button.Size = new Size(209, 79);
-            Button.TabIndex = 6;
-            Button.Text = "Сгенерировать\r\nпароль\r\n";
-            Button.UseVisualStyleBackColor = true;
-            Button.Click += Button_Click;
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            button1.Location = new Point(12, 218);
+            button1.Name = "button1";
+            button1.Size = new Size(198, 97);
+            button1.TabIndex = 3;
+            button1.Text = "Сгенерировать пароль";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // button2
             // 
-            button2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
-            button2.Location = new Point(12, 238);
+            button2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            button2.Location = new Point(12, 321);
             button2.Name = "button2";
-            button2.Size = new Size(209, 69);
-            button2.TabIndex = 7;
+            button2.Size = new Size(200, 111);
+            button2.TabIndex = 4;
             button2.Text = "Удалить запись";
             button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
-            // TextBox
+            // tbusserlog
+            // 
+            tbusserlog.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            tbusserlog.Location = new Point(12, 68);
+            tbusserlog.Multiline = true;
+            tbusserlog.Name = "tbusserlog";
+            tbusserlog.Size = new Size(200, 130);
+            tbusserlog.TabIndex = 5;
+            // 
+            // lblog
+            // 
+            lblog.FormattingEnabled = true;
+            lblog.Location = new Point(218, 68);
+            lblog.Name = "lblog";
+            lblog.Size = new Size(168, 364);
+            lblog.TabIndex = 6;
+            // 
+            // lbpass
+            // 
+            lbpass.FormattingEnabled = true;
+            lbpass.Location = new Point(394, 68);
+            lbpass.Name = "lbpass";
+            lbpass.Size = new Size(206, 364);
+            lbpass.TabIndex = 7;
+            // 
+            // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(601, 317);
+            ClientSize = new Size(615, 450);
+            Controls.Add(lbpass);
+            Controls.Add(lblog);
+            Controls.Add(tbusserlog);
             Controls.Add(button2);
-            Controls.Add(Button);
-            Controls.Add(ListBox);
-            Controls.Add(ComboBox);
-            Controls.Add(listBox1);
+            Controls.Add(button1);
             Controls.Add(textBox3);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
-            Name = "TextBox";
-            Text = "Менеджер паролей";
+            Name = "Form1";
+            Text = "Form1";
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private ListBox listBox1;
-        private ListBox ComboBox;
-        private ListBox ListBox;
-        private Button Button;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button1;
         private Button button2;
+        private TextBox tbusserlog;
+        private ListBox lblog;
+        private ListBox lbpass;
     }
 }
--- Lesson28-2(DZ)/Form1.cs
+++ Lesson28-2(DZ)/Form1.cs
@@ -1,16 +1,34 @@
+using System.Text;
+
 namespace Lesson28_2_DZ_
 {
-    public partial class TextBox : Form
+    public partial class Form1 : Form
     {
-        public TextBox()
+        public Form1()
         {
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
         {
+            lblog.Items.RemoveAt(lbpass.SelectedIndex);
+            lbpass.Items.RemoveAt(lbpass.SelectedIndex);
+        }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string validChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
+            StringBuilder password = new StringBuilder();
+            Random random = new Random();
+
+            int length = random.Next(5, 20);
+
+            for (int i = 0; i < length; i++)
+            {
+                password.Append(validChars[random.Next(validChars.Length)]);
+            }
+            lblog.Items.Add(tbusserlog.Text);
+            lbpass.Items.Add(password.ToString());
         }
     }
 }
-   
