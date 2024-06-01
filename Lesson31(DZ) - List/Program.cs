2594c4555ccf664f30f5b581bea0fa72c885a794
IrinkaCode <pil02022012@gmail.com>
2024-06-01 18:39:09 +0200

    30,05

---
+++ Lesson31(DZ) - List/Program.cs
@@ -0,0 +1,85 @@
+﻿//Задача: Учет сотрудников в компании.
+//Разработайте программу для учета сотрудников в компании. Каждый сотрудник должен иметь имя, фамилию и должность. Реализуйте следующие функции:
+//Добавление нового сотрудника в список.
+//Удаление сотрудника по его фамилии.
+//Поиск сотрудника по его имени или должности.
+//Вывод списка всех сотрудников.
+//Подсчет количества сотрудников в компании.
+
+using System;
+using System.Collections.Generic;
+
+public class Employee
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Position { get; set; }
+
+    public Employee(string firstName, string lastName, string position)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Position = position;
+    }
+}
+
+public class EmployeeManagement
+{
+    private List<Employee> employees = new List<Employee>();
+
+    public void AddEmployee(string firstName, string lastName, string position)
+    {
+        employees.Add(new Employee(firstName, lastName, position));
+    }
+
+    public void RemoveEmployeeByLastName(string lastName)
+    {
+        employees.RemoveAll(e => e.LastName == lastName);
+    }
+
+    public List<Employee> FindEmployeesByNameOrPosition(string query)
+    {
+        return employees.FindAll(e => e.FirstName == query || e.Position == query);
+    }
+
+    public void PrintAllEmployees()
+    {
+        foreach (var employee in employees)
+        {
+            Console.WriteLine($"Имя: {employee.FirstName} {employee.LastName}, Должность: {employee.Position}");
+        }
+    }
+
+    public int CountEmployees()
+    {
+        return employees.Count;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        EmployeeManagement management = new EmployeeManagement();
+
+        management.AddEmployee("Ivan", "Ivanov", "Developer");
+        management.AddEmployee("Jana", "Smirnova", "Manager");
+        management.AddEmployee("Alisa", "Popova", "Designer");
+
+        Console.WriteLine("Все работники:");
+        management.PrintAllEmployees();
+
+        Console.WriteLine("\nКоличество сотрудников: " + management.CountEmployees());
+
+        Console.WriteLine("\nНайти сотрудников по имени 'Ivan':");
+        List<Employee> foundEmployees = management.FindEmployeesByNameOrPosition("Ivan");
+        foreach (var employee in foundEmployees)
+        {
+            Console.WriteLine($"Имя: {employee.FirstName} {employee.LastName}, Должность: {employee.Position}");
+        }
+
+        Console.WriteLine("\nУдалить сотрудника по фамилии 'Smirnova':");
+        management.RemoveEmployeeByLastName("Smirnova");
+        management.PrintAllEmployees();
+    }
+}
--- Lesson40-1/Program.cs
+++ Lesson40-1/Program.cs
@@ -1,26 +1,188 @@
-﻿//Деревья 
+﻿////Деревья 
 
-Console.Write("Введите путь: ");
-PrintDirectory(new DirectoryInfo(Console.ReadLine()!), "", true);
+//Console.Write("Введите путь: ");
+//PrintDirectory(new DirectoryInfo(Console.ReadLine()!), "", true);
 
 
-void PrintDirectory(DirectoryInfo directory, string indent, bool lastDirectory)
+//void PrintDirectory(DirectoryInfo directory, string indent, bool lastDirectory)
+//{
+//    Console.Write(indent);
+//    if (lastDirectory)
+//    {
+//        Console.Write("|--");
+//        indent += " ";
+//    }
+//    else
+//    {
+//        Console.Write("|--");
+//        indent += "| ";
+//    }
+//    Console.WriteLine(directory.Name);
+//    DirectoryInfo[] subDirectories = directory.GetDirectories();
+//    for (int i = 0; i < subDirectories.Length; i++)
+//    {
+//        PrintDirectory(subDirectories[i], indent, i == subDirectories.Length - 1);
+//    }
+//}
+
+
+////Поля класса - 30.05
+//namespace Test_1_Bogdan
+//{
+//    class Program
+//    {
+//        static int number = 50; // поле класса (глобальная переменная)
+
+//        public static void Main(string[] args)
+//        {
+//            PersonData newPerson = new PersonData();
+//            newPerson.SetName("; DROP DATABASE;");
+//            newPerson.petName = "Черчилль";
+//            newPerson.SetAge(25);
+
+//            Console.WriteLine("Данные успешно сохранены на сервере");
+//        }
+//    }
+
+//    class PersonData
+//    {
+//        private string _name;
+//        public string petName;
+//        private int _age;
+
+//        public void SetName(string newName)
+//        {
+//            if (!newName.Contains("DROP"))
+//            {
+//                _name = newName;
+//            }
+//            else
+//            {
+//                throw new ArgumentException("ПРАВИЛЬНО ОБРАБАТЫВАЙ ДАННЫЕ!");
+//            }
+//        }
+
+//        public int GetAge()
+//        {
+//            return _age;
+//        }
+
+//        public void SetAge(int newAge)
+//        {
+//            if (newAge < 120 && newAge > 0)
+//            {
+//                _age = newAge;
+//            }
+//            else
+//            {
+//                throw new ArgumentException("ПРАВИЛЬНО ОБРАБАТЫВАЙ ДАННЫЕ!");
+//            }
+//        }
+//    }
+//}
+
+//Сортировка пузырьком
+
+//int[] array = new int[] { 2, 8, 3, 15, 46, 1 };
+//int arrLenght = array.Length - 1;
+//for (int i = 0; i < arrLenght; i++) //количество обходов массива
+//{
+//	for (int j = 0; j < arrLenght; j++)
+//	{
+//		if (array[j] > array[j+1])
+//		{
+//			int temp=array[j];   //сохраняем в промежуточную переменную
+//			array[j]=array[j+1]; //меняем местами
+//			array[j+1]=temp;      //устанавливаем число обратно в  массив
+//		}
+//	}
+//}
+//Console.WriteLine();
+
+
+
+////Сортировка выбором
+using System.Diagnostics;
+
+namespace Test_1_Bogdan
 {
-    Console.Write(indent);
-    if (lastDirectory)
+    class Program
     {
-        Console.Write("|--");
-        indent += " ";
-    }
-    else
-    {
-        Console.Write("|--");
-        indent += "| ";
-    }
-    Console.WriteLine(directory.Name);
-    DirectoryInfo[] subDirectories = directory.GetDirectories();
-    for (int i = 0; i < subDirectories.Length; i++)
-    {
-        PrintDirectory(subDirectories[i], indent, i == subDirectories.Length - 1);
+
+        public static int[] bubble;
+        public static int[] selection;
+
+        public static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+            CreateRandomArr(50000);
+
+            sw.Start();
+            BubbleSort(bubble);
+            sw.Stop();
+
+            long timeBubbleSort = sw.ElapsedMilliseconds;
+
+            sw.Start();
+            SelectionSort(selection);
+            sw.Stop();
+
+            long timeSelectionSort = sw.ElapsedMilliseconds;
+
+            Console.WriteLine("Время сортировки пузырьком: " + timeBubbleSort);
+            Console.WriteLine("Время сортировки выбором: " + timeSelectionSort);
+        }
+
+        public static void BubbleSort(int[] array)
+        {
+            int arrLength = array.Length - 1;
+
+            for (int i = 0; i < arrLength; i++) // Количество обходов массива
+            {
+                for (int j = 0; j < arrLength; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j]; // Сохраняем в промеж. переменную
+                        array[j] = array[j + 1]; // Меняем местами
+                        array[j + 1] = temp; // Устанавливаем число обратно в массив
+                    }
+                }
+            }
+            Console.WriteLine("Пузырек все");
+        }
+
+        public static void SelectionSort(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int minNumberIndex = i;
+
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[minNumberIndex])
+                    {
+                        minNumberIndex = j;
+                    }
+                }
+                int temp = array[minNumberIndex];
+                array[minNumberIndex] = array[i];
+                array[i] = temp;
+            }
+        }
+        public static void CreateRandomArr(int lenght)
+        {
+            Random rnd = new Random();
+
+            bubble = new int[lenght];
+            selection = new int[lenght];
+
+            for (int i = 0; i < lenght; i++)
+            {
+                int temp = rnd.Next(int.MinValue, int.MaxValue);
+                bubble[i] = temp;
+                bubble[i] = temp;
+            }
+        }
     }
 }
