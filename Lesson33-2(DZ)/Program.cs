7bc3c784b5b51c86dbca4b1f4d6e336a52691583
IrinkaCode <pil02022012@gmail.com>
2024-06-06 16:34:21 +0200

    06/06-1

---
+++ Lesson33-2(DZ)/Program.cs
@@ -0,0 +1,54 @@
+﻿//Задача 1
+
+//using System;
+
+//int[] arr = { 5, 2, 8, 1, 9 };
+//BubbleSort(arr);
+//Console.Write("Отсортированный массив в обратном порядке: ");
+//foreach (int num in arr)
+//{
+//    Console.Write(num + " ");
+//}
+
+//static void BubbleSort(int[] arr)
+//{
+//    int n = arr.Length;
+//    for (int i = 0; i < n - 1; i++)
+//    {
+//        for (int j = 0; j < n - i - 1; j++)
+//        {
+//            if (arr[j] < arr[j + 1])
+//            {
+//                int temp = arr[j];
+//                arr[j] = arr[j + 1];
+//                arr[j + 1] = temp;
+//            }
+//        }
+//    }
+//}
+
+
+
+//задача 2
+//Вот реализация алгоритма сортировки на C#, который сортирует список чисел таким образом, что четные числа идут в начале, а нечетные в конце:
+
+List<int> numbers = new List<int> { 5, 2, 8, 1, 9, 3, 6, 4, 7 };
+
+numbers.Sort((a, b) =>
+{
+    if (a % 2 == 0 && b % 2 == 0)
+        return a.CompareTo(b);
+
+    if (a % 2 != 0 && b % 2 != 0)
+        return a.CompareTo(b);
+
+    // Если одно число четное, а другое нечетное, то четное идет раньше
+    if (a % 2 == 0)
+        return -1;
+    else
+        return 1;
+});
+
+foreach (int num in numbers)
+    Console.Write(num + " ");
+    
